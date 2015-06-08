492cc61c5371a5206b2c9c1abc92e8e06263ea5d
Stuart Wheater <stuart.wheater@arjuna.com>
2015-06-08 16:52:07 +0100

    Added 'Sheet To CSV' factory

--- apachepoi-plugin/src/main/java/com/arjuna/dbplugins/apachepoi/ApachePOIDataFlowNodeFactoriesSetup.java
+++ apachepoi-plugin/src/main/java/com/arjuna/dbplugins/apachepoi/ApachePOIDataFlowNodeFactoriesSetup.java
@@ -5,7 +5,6 @@
 package com.arjuna.dbplugins.apachepoi;
 
 import java.util.Collections;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
@@ -14,6 +13,7 @@ import javax.ejb.Startup;
 import com.arjuna.databroker.data.DataFlowNodeFactory;
 import com.arjuna.databroker.data.DataFlowNodeFactoryInventory;
 import com.arjuna.dbplugins.apachepoi.xssf.XSSFRowToJSONDataFlowNodeFactory;
+import com.arjuna.dbplugins.apachepoi.xssf.XSSFSheetToCSVDataFlowNodeFactory;
 
 @Startup
 @Singleton
@@ -22,15 +22,18 @@ public class ApachePOIDataFlowNodeFactoriesSetup
     @PostConstruct
     public void setup()
     {
-        DataFlowNodeFactory xssfRowToJSONDataFlowNodeFactory = new XSSFRowToJSONDataFlowNodeFactory("Apache POI XSSF Row To JSON Data Flow Node Factories", Collections.<String, String>emptyMap());
+        DataFlowNodeFactory xssfRowToJSONDataFlowNodeFactory  = new XSSFRowToJSONDataFlowNodeFactory("Apache POI XSSF Row To JSON Data Flow Node Factories", Collections.<String, String>emptyMap());
+        DataFlowNodeFactory xssfSheetToCSVDataFlowNodeFactory = new XSSFSheetToCSVDataFlowNodeFactory("Apache POI XSSF Sheet To CSV Data Flow Node Factories", Collections.<String, String>emptyMap());
 
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(xssfRowToJSONDataFlowNodeFactory);
+        _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(xssfSheetToCSVDataFlowNodeFactory);
     }
 
     @PreDestroy
     public void cleanup()
     {
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Apache POI XSSF Row To JSON Data Flow Node Factories");
+        _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Apache POI XSSF Sheet To CSV Data Flow Node Factories");
     }
 
     @EJB(lookup="java:global/databroker/data-core-jee/DataFlowNodeFactoryInventory")
